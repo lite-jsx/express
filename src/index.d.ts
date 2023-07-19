a6327b8bef46db8a1da9080966478c135f7f107b
Daniel Prates <danprates94@gmail.com>
2023-07-19 00:11:03 -0300

    feat: add declarion file

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,13 +1,51 @@
-import { NextFunction, Request, Response } from "express";
+import type { Request, Response, NextFunction } from "express";
+
+// newer versions of `@types/express`
+declare module "express-serve-static-core" {
+  interface Response {
+    /**
+     * Renders a template or JSX component as a view.
+     *
+     * @param template - The template or JSX component to render.
+     * @param data - Optional data to pass to the template or JSX component.
+     * @returns void
+     *
+     * @example
+     * // Render a JSX component
+     * res.render(Home, { message: "Hello, World!" });
+     *
+     * // Render a raw template
+     * res.render(h("div", null, h("h1", null, message)), { message: "Hello, World!" });
+     */
+    render(template: string | Function, data?: object): void;
+  }
+}
+
+// older versions of `@types/express`
+declare module "express" {
+  interface Response {
+    /**
+     * Renders a template or JSX component as a view.
+     *
+     * @param template - The template or JSX component to render.
+     * @param data - Optional data to pass to the template or JSX component.
+     * @returns void
+     *
+     * @example
+     * // Render a JSX component
+     * res.render(Home, { message: "Hello, World!" });
+     *
+     * // Render a raw template
+     * res.render(h("div", null, h("h1", null, message)), { message: "Hello, World!" });
+     */
+    render(template: string | Function, data?: object): void;
+  }
+}
 
 declare namespace LiteJSX {
   /**
    * Middleware that uses lite-jsx to render JSX templates in an Express application.
    *
-   * @param req The Express request object.
-   * @param res The Express response object.
-   * @param next The Express next function.
-   * @returns {void}
    * @example
    * const express = require("express");
    * const app = express();
@@ -32,3 +70,5 @@ declare namespace LiteJSX {
     next: NextFunction
   ): void;
 }
+
+export = LiteJSX;
